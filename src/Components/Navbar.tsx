cb7a1212eb37abf1252d99f5f6d0496b13e0d0fb
Jacob.Ryabinky@mpfi.org <mrjacobry@gmail.com>
2023-06-29 21:24:30 -0400

    basic setup

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+import './css/navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        {/* <!-- HTML for the navbar -->  */}
+        <ul className="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
+
+
