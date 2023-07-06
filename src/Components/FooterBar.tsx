02549665cec0b958c85a24dfefb4f6bfecc1fb17
hydrol0x <mrjacobry@gmail.com>
2023-07-06 00:56:59 -0400

    changed social iamges urls to github uploaded

--- src/Components/FooterBar.tsx
+++ src/Components/FooterBar.tsx
@@ -6,8 +6,8 @@ const FooterBar = () => {
   return (
     <div className='footerbar_wrapper'>
         <ul className="footerbar">
-            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-ryabinky-ba573626b"><img src='src\assets\LI-In-Bug.png' height={25} width={25}/></a></li>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/hydrol0x"><img src='src\assets\github-mark-white.png' height={25} width={25}/></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-ryabinky-ba573626b"><img src='https://github.com/hydrol0x/hydrol0x.github.io/assets/34951139/e386a7b0-38da-4331-9c99-df9f5f6a8616' height={25} width={25}/></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/hydrol0x"><img src='https://github.com/hydrol0x/hydrol0x.github.io/assets/34951139/b45060ae-a28a-4db7-a8d6-d8ffe7841b47' height={25} width={25}/></a></li>
         </ul>
     </div>
   )
