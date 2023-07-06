8302fce70f47f8e2e0e8a1520c45043b960ae3d0
hydrol0x <mrjacobry@gmail.com>
2023-07-06 00:46:03 -0400

    socials links

--- src/Components/FooterBar.tsx
+++ src/Components/FooterBar.tsx
@@ -6,8 +6,8 @@ const FooterBar = () => {
   return (
     <div className='footerbar_wrapper'>
         <ul className="footerbar">
-            <li><a href="#about"><img src='src\assets\LI-In-Bug.png' height={25} width={25}/></a></li>
-            <li><a href="#about"><img src='src\assets\github-mark-white.png' height={25} width={25}/></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-ryabinky-ba573626b"><img src='src\assets\LI-In-Bug.png' height={25} width={25}/></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.github.com/hydrol0x"><img src='src\assets\github-mark-white.png' height={25} width={25}/></a></li>
         </ul>
     </div>
   )
