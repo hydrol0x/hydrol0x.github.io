b29d51fc682c29b3f3f909b04529919251c06042
hydrol0x <mrjacobry@gmail.com>
2023-07-06 01:14:42 -0400

    changed logo, favicon

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <li><a href="#projects">Projects</a></li>
             <li><a href="#experience">Experience</a></li>
             <li><a href="#about">About</a></li>
-            <li className='logo'><a href="#home">JR</a></li>
+            <li className='logo'><a href="#home"><img className="logo" height={30} width={30} src='https://github.com/hydrol0x/hydrol0x.github.io/assets/34951139/9b2df076-e340-4923-b64a-7365007bf688'/></a></li>
         </ul>
     </div>
   )
