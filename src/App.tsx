faa38f8dd7d4f806ad22e3dbe50a11c4c26c62c1
Jacob.Ryabinky@mpfi.org <mrjacobry@gmail.com>
2023-07-10 20:40:08 -0400

    fixed for smaller display styling

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ function App() {
       <Navbar />
       <section className='section1' id="home">
         <div className='flexbox-high'>
-          <div>
+          <div className='greeting_title'>
               <h1 className='greeting_1'>Hey, I'm</h1>
               {/* <h1 className='greeting_2'>JACOB RYABINKY</h1> */}
             <GradShadowHeading text={"JACOB RYABINKY"} gradColors={gradColors1} length={800}/>
@@ -67,22 +67,20 @@ function App() {
       <section className='section2' id="about">
         <div className='flexbox-veryhigh'>
           <div>
-              {/* <h1 className='greeting_2'>JACOB RYABINKY</h1> */}
             <GradShadowHeading text={"About me"} gradColors={gradColors2} length={800}/>
           </div>
-          <p className='about_paragraph'>
+          <p className='info_para'>
           As a dedicated student, my relentless pursuit of knowledge in the fields of STEM, particularly programming and computer science, has shaped my academic journey. I am deeply passionate about leveraging technology to drive innovation and solve complex problems. With a keen focus on programming, I have actively engaged in a diverse range of STEM projects that have allowed me to explore fascinating realms. From delving into the intricate world of nuclear physics to unraveling the mysteries of the human brain in neuroscience, my experiences have enriched my understanding and honed my skills. 
           </p> 
-        </div> 
         <img src='src\assets\testpictures.png' className='aboutpics'/>
+        </div> 
       </section>
       <section className='section3' id='experience'>
         <div className='flexbox-low'>
           <div>
-              {/* <h1 className='greeting_2'>JACOB RYABINKY</h1> */}
             <GradShadowHeading text={"Experience"} gradColors={gradColors3} length={800}/>
           </div>
-          <p className='about_paragraph'>
+          <p className='info_para'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
           </p> 
         </div> 
@@ -90,10 +88,9 @@ function App() {
       <section className='section4' id='projects'>
         <div className='flexbox-low'>
           <div>
-              {/* <h1 className='greeting_2'>JACOB RYABINKY</h1> */}
             <GradShadowHeading text={"Projects"} gradColors={gradColors4} length={800}/>
           </div>
-          <p className='about_paragraph'>
+          <p className='info_para'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
           </p> 
         </div> 
@@ -101,10 +98,9 @@ function App() {
       <section className='section5' id='contact'>
         <div className='flexbox-low'>
           <div>
-              {/* <h1 className='greeting_2'>JACOB RYABINKY</h1> */}
             <GradShadowHeading text={"Contact"} gradColors={gradColors5} length={800}/>
           </div>
-          <p className='about_paragraph'>
+          <p className='info_para'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
           </p> 
         </div>  
