862e4f59dcebd29fb5d972fc176e05dec0c3dc52
hydrol0x <mrjacobry@gmail.com>
2023-12-18 18:20:14 -0500

    css, navbar, meta desc, title

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,39 @@
-import './css/navbar.css'
+import "./css/navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='navbar_wrapper'>
-        {/* <!-- HTML for the navbar -->  */}
-        <ul className="navbar">
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#about">About</a></li>
-            <li className='logo'><a href="#home"><img className="logo" height={30} width={30} src='https://github.com/hydrol0x/hydrol0x.github.io/assets/34951139/9b2df076-e340-4923-b64a-7365007bf688'/></a></li>
-        </ul>
+    <div className="navbar_wrapper">
+      {/* <!-- HTML for the navbar -->  */}
+      <ul className="navbar">
+        <li>
+          <a href="https://blog.jacobryabinky.com">Blog</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li className="logo">
+          <a href="#home">
+            <img
+              className="logo"
+              height={30}
+              width={30}
+              src="https://github.com/hydrol0x/hydrol0x.github.io/assets/34951139/9b2df076-e340-4923-b64a-7365007bf688"
+            />
+          </a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
-
+export default Navbar;
